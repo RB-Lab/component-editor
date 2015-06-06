a73ce45f7df57dcc1b1cbbe3101d8e91ed94b7d3
bertolomych <rom.bekk@gmail.com>
2015-06-06 03:31:22 +0300

    better styles editor

--- src/js/components/props-pane/component.jsx
+++ src/js/components/props-pane/component.jsx
@@ -1,8 +1,33 @@
 const React = require('react');
 const {Button, Input} = require('react-bootstrap');
+var AceEditor  = require('react-ace');
+require('brace/mode/css');
+var camelcase = require('camelcase');
+var decamelize = require('decamelize');
 
+function toCss(style){
+	let result = '{\n';
+	for(var key in style){
+		result += `\t${decamelize(key, '-')}: ${style[key]};\n`;
+	}
+	return result + '}\n';
+}
+
+function toObject(style){
+	style = style.match(/{(.|\s)+}/gm)[0].replace(/{|}/g, '');
+	return style.split('\n').reduce((res, line) => {
+		let lineArr = line.split(':');
+		if(lineArr.length !== 2){
+			return res;
+		}
+		res[camelcase(lineArr[0].trim())] = lineArr[1].replace(';', '').trim();
+		return res;
+	}, {});
+}
 
 let Pane = React.createClass({
+
+	currentComponentStyle: '',
 
 	getInitialState(){
 		return this.createState();
@@ -19,7 +44,11 @@ let Pane = React.createClass({
 		}
 		let state = {};
 		for(var i in props.component.props){
-			state[i] = JSON.stringify(props.component.props[i]);
+			if(i === 'style'){
+				state[i] = toCss(props.component.props[i]);
+				continue;
+			}
+			state[i] = props.component.props[i];
 		}
 		return state;
 	},
@@ -30,14 +59,33 @@ let Pane = React.createClass({
 
 		var fillTheProp = (prop) => {
 			return (e) => {
+				if(prop === 'style') {
+					this.currentComponentStyle = e;
+					return;
+				}
 				this.setState({[prop]: e.target.value});
 			};
 		};
 
 		for(var i in this.state){
-			var type = typeof this.props.component.props[i] === 'object' ? 'textarea' : 'text';
+			if(i === 'style'){
+				var name = this.props.component.element.displayName;
+				this.propInputs.push(
+				<div>
+					<b>style</b>
+					<AceEditor
+						mode='css'
+						width='100%'
+						height='8em'
+						value={name + this.state[i]}
+						onChange={fillTheProp(i)}
+						name='style-editor'/>
+				</div>
+				);
+				continue;
+			}
 			this.propInputs.push(<Input
-							type={type}
+							type='text'
 							value={this.state[i]}
 							label={i}
 							onChange={fillTheProp(i)}/>);
@@ -45,31 +93,39 @@ let Pane = React.createClass({
 	},
 
 	save(){
-		var newProps = {};
+		let newProps = {};
 		for(var i in this.state){
 			if(this.state[i]){
-				newProps[i] = JSON.parse(this.state[i]);
+				if(i === 'style'){
+					newProps.style = toObject(this.currentComponentStyle);
+					continue;
+				}
+				newProps[i] = this.state[i];
 			}
 		}
 		this.props.save(newProps);
 	},
 
 	render() {
+		let style = {
+			width: '20%',
+			float: 'left',
+			marginTop: '-43px',
+			height: '100%',
+			borderRight: '1px solid #d0d0d0',
+			backgroundColor: '#F7F7F7',
+			padding: '43px 10px 0'
+		};
+		if(!this.props.component){
+			return <aside style={style} />;
+		}
 		this.fillPropInputs();
 		return (
-			<aside style={{
-					width: '20%',
-					float: 'left',
-					marginTop: '-43px',
-					height: '100%',
-					borderRight: '1px solid #d0d0d0',
-					backgroundColor: '#F7F7F7',
-					padding: '43px 10px 0'
-				}}>
+			<aside style={style}>
+				<h3>{this.props.component.element.displayName}</h3>
 				<h4>Props</h4>
 				{this.propInputs}
 				<h4>Logic</h4>
-				<p>Logic description will be here with CodeMirror</p>
 				<Button onClick={this.save}>Save</Button>
 			</aside>
 		);
